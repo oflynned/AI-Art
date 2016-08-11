3de2b911d2e7574bec0f148b5c38482446859db2
gavkr <gavkr.ire@gmail.com>
2016-08-11 16:32:52 +0100

    Changes added for dynamic clustering of input data

--- aiart/Screen.py
+++ aiart/Screen.py
@@ -1,26 +1,42 @@
 import pygame
 import random
+import numpy as np
 
 
 class Screen(object):
     WIDTH = 640
     HEIGHT = 480
-    WHITE = (254, 254, 254)
-    BOX_SIZE = 5
+    WHITE = (255, 255, 255)
 
     def __init__(self):
         self.display = pygame.display.set_mode((Screen.WIDTH, Screen.HEIGHT), 0, 24)
+        # pygame.display.set_caption("AI Art")
         self.display.fill(Screen.WHITE)
-        self.square = pygame.Surface((Screen.BOX_SIZE, Screen.BOX_SIZE))
+        self.square = pygame.Surface((1, 1))
+        self.imageArray = np.zeros((Screen.WIDTH, Screen.HEIGHT), dtype=np.uint8)
+        # self.x = 0
+        # self.y = 0
+        # self.row = 0
+        # self.col = 0
 
-    def draw(self, color):
-        x = random.randint(0, Screen.WIDTH)
-        y = random.randint(0, Screen.HEIGHT)
+    # def draw(self, color):
+    #     x = random.randint(0, 100)
+    #     y = random.randint(0, 100)
+    #     self.display.set_at((x, y), color)
+    #     draw_me = pygame.Rect(x, y, 1, 1)
+    #     self.display.blit(self.square, draw_me)
+    #     pygame.display.update()
 
-        draw_me = pygame.Rect(x, y, Screen.BOX_SIZE, Screen.BOX_SIZE)
+    def draw(self, X, Y, color):
+        x = random.randint(0, Screen.HEIGHT)
+        y = random.randint(0, Screen.WIDTH)
+        # self.display.set_at((self.x, self.y), color)
+        draw_me = pygame.Rect(X, Y, 7, 7)
+        # self.x += 1
+        # self.y += 1
+        # self.display.blit(self.square, draw_me)
         pygame.draw.rect(self.display, color, draw_me)
-
         pygame.display.update()
 
-    def returnSurfaceArray(self):
+    def fillImageArray(self, x, y):
         pass
--- aiart/multilayer_perceptron.py
+++ aiart/multilayer_perceptron.py
@@ -1,19 +1,17 @@
 import tensorflow as tf
-from DataGenerator import *
-from ColorMapping import *
-from Screen import *
-
-# Parameters
-learning_rate = 0.001
-training_epochs = 15
-batch_size = 784
-display_step = 1
+from aiart.DataGenerator import *
+from aiart.ColorMapping import *
+from aiart.Screen import *
+# from DataGenerator import *
+# from ColorMapping import *
+# from Screen import *
+import numpy as np
 
 # Network Parameters
 n_hidden_1 = 256 # 1st layer number of features
 n_hidden_2 = 256 # 2nd layer number of features
 n_input = 256
-n_classes = 1 # total classes (0-9 digits)
+n_classes = 5 # total classes (0-9 digits)
 
 # tf Graph input
 x = tf.placeholder("float", [None, n_input])
@@ -24,7 +22,7 @@ y = tf.placeholder("float", [None, n_classes])
 def multilayer_perceptron(x, weights, biases):
     # Hidden layer with RELU activation
     layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    layer_1 = tf.nn.tanh(layer_1)
+    layer_1 = tf.nn.sigmoid(layer_1)
     # Hidden layer with RELU activation
     layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
     layer_2 = tf.nn.relu(layer_2)
@@ -47,20 +45,32 @@ biases = {
 # Initializing the variables
 init = tf.initialize_all_variables()
 
-d = DataGenerator()
+# d = DataGenerator()
 screen = Screen()
-d.generateData()
+# d.generateData()
+# sensor_data = d.getSensorData()
+sensor_data = []
+data = open('data.csv', 'r').readlines()
+for line in data:
+    temp = []
+    splitted = line.split(',')
+    for elem in splitted:
+        temp.append(int(elem))
+    sensor_data.append(list(temp))
 
-while True:
-    sensor_data = d.getSensorData()
-    dv = tf.placeholder(tf.float32, None)
-    temp = tf.reshape(dv, [1, n_input])
-    with tf.Session() as s:
-        s.run(init)
-        for dt in range(len(sensor_data)):
-            n = s.run(temp, feed_dict={dv: sensor_data[dt]})
-            out = s.run(multilayer_perceptron(n, weights, biases))
-            val = int(out) % 80
-            color = ColorMapping.color_map(val)
-            screen.draw(color)
-            # print(screen.returnSurfaceArray())
+dv = tf.placeholder(tf.float32, None)
+temp = tf.reshape(dv, [1, n_input])
+with tf.Session() as s:
+    s.run(init)
+    for dt in range(len(sensor_data)):
+        # print(sensor_data[dt])
+        n = s.run(temp, feed_dict={dv: sensor_data[dt]})
+        out = s.run(multilayer_perceptron(n, weights, biases))
+        X = int(out[0][0] % screen.WIDTH)
+        Y = int(out[0][1] % screen.HEIGHT)
+        R = int(out[0][2] % 255)
+        G = int(out[0][3] % 255)
+        B = int(out[0][4] % 255)
+        print(X, Y, R, G, B)
+        color = (R, G, B)
+        screen.draw(X, Y, color)
